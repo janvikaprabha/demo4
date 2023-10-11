f4f19f3108fb8c077c10d185715111c4696e435d
Athira S <athirazgirish@gmail.com>
2023-10-11 22:28:32 +0530

    added

---
+++ travelproject/credentials/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .import views
+from django.urls import path
+
+from travelproject import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+    path('register',views.register,name='register'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout')
+
+]
